67888a049b27c64869c1834c2fbc1b3185a749dc
JohnPss <joaopedrosilvasiqueira1@gmail.com>
2025-08-25 14:25:26 -0300

    Add: Problems 1769. and 2807.

---
+++ problems/1769.MinimumNumberOfOperationsToMoveAllBallsToEachBox.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> minOperations(string boxes)
+    {
+        vector<int> answer;
+        for (int i = 0; i < boxes.length(); i++)
+        {
+            int x = 0;
+            for (int j = 0; j < boxes.length(); j++)
+            {
+                if (i != j && boxes[j] == '1')
+                {
+                    x += abs(i - j);
+                }
+            }
+            answer.push_back(x);
+        }
+        return answer;
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    vector<int> result = solution.minOperations("110");
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i];
+        if (i < result.size() - 1)
+            cout << " ";
+    }
+    cout << endl;
+}
